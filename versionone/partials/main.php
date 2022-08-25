a75adad71a6b70bece7db099fe9ac024050d39f6
LucaSpennato <luca.spennato@gmail.com>
2022-08-25 17:26:33 +0200

    var created in foreach, printed in html

--- versionone/partials/main.php
+++ versionone/partials/main.php
@@ -2,8 +2,40 @@
     include __DIR__ . '/../../db/database.php';
 ?>
 
-<main>
-    <?php
-        echo $disks[0]['title'];
-    ?>
+<main class="bg-dark">
+    <div class="container">
+        <div class="row col-10 m-auto py-5">
+
+            <?php foreach ($disks as $disk) { 
+                $title = $disk['title'];
+                $img = $disk['poster'];
+                $author = $disk['author'];
+                $genre = $disk['genre'];
+                $year = $disk['year'];  
+            ?>
+
+                <div class="col-2 text-light">
+
+                    <img class="img-fluid" src="<?php echo $img; ?>" 
+                        alt="<?php echo $author . "'s song"; ?>"
+                    >
+
+                    <h5>
+                        <?php echo $title; ?>
+                    </h5>
+
+                    <div>
+                        <?php echo $author ?>
+                    </div>
+                    <div>
+                        <?php echo $year ?>
+                    </div>
+
+                </div>
+
+            <?php } ?>
+        </div>
+    </div>
+
+
 </main>
