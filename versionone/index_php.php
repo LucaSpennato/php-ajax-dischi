f3722526319336e6a76b12117c976cea18cbc374
LucaSpennato <luca.spennato@gmail.com>
2022-08-25 17:51:45 +0200

    header styled

--- versionone/index_php.php
+++ versionone/index_php.php
@@ -16,7 +16,7 @@
 
     <?php
         include __DIR__ . '/partials/header.php';
-        include __DIR__ . '/partials/main.php'
+        include __DIR__ . '/partials/main.php';
     ?>
 
 </body>
--- versionone/partials/header.php
+++ versionone/partials/header.php
@@ -1,3 +1,5 @@
 <header>
-    Ciao sono l'header
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="brand logo">
+    </div>
 </header>
